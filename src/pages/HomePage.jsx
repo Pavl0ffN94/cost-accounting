d02f87b05bca4ad58a588ab005488de4f916f5d3
Николай Павлов <n.pavlov@i-sys.ru>
2023-08-23 10:17:07 +0400

    cleaning

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,16 @@
 import React, {useCallback, memo} from 'react';
 import {Navigate} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
-import {setCurrentUser, selectCurrentUser} from '../store/slices/usersSlice'; // Импортируем селектор для текущего пользователя
+import {setCurrentUser, selectCurrentUser} from '../store/slices/usersSlice';
 import {Costs} from '../components/Costs/Costs';
 import {NewCost} from '../components/NewCost/NewCost';
 
 const HomePageImpl = () => {
   const dispatch = useDispatch();
-  const currentUser = useSelector(selectCurrentUser); // Используем селектор для текущего пользователя
+  const currentUser = useSelector(selectCurrentUser);
 
   const logout = useCallback(() => {
-    dispatch(setCurrentUser(null)); // Сбрасываем текущего пользователя
+    dispatch(setCurrentUser(null));
   }, [dispatch]);
 
   return currentUser ? (
